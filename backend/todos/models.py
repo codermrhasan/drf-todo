5985fe2a80eefc5af1ae4e64959680a7b0dea05e
Rakib Hasan <hasanthecoder@gmail.com>
2019-10-22 21:58:11 +0600

    creating todo model

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    text = models.CharField(max_length=100)
+    status = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created']
