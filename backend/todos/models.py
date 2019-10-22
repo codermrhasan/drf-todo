90a21676cde933afdeb9e37a346926fb8ab2f2e3
Rakib Hasan <hasanthecoder@gmail.com>
2019-10-22 23:16:54 +0600

    adding owner(user) field in todo models

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Todo(models.Model):
+    owner = models.ForeignKey(
+        'auth.User', related_name='todos', on_delete=models.CASCADE)
     text = models.CharField(max_length=100)
     status = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now=True)
