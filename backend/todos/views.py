28a37a74a8ee7e9d951902003a060b8417ce7a3f
Rakib Hasan <hasanthecoder@gmail.com>
2019-10-22 23:03:38 +0600

    Refactoring views to generinc class-based views

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,34 +1,13 @@
 from todos.models import Todo
 from todos.serializers import TodoSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class TodoList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+class TodoList(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class TodoDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
