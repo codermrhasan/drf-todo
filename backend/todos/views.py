917d1b904daf8f9d1facd950d3208c8f9e9045d9
Rakib Hasan <hasanthecoder@gmail.com>
2019-10-23 00:48:06 +0600

    refactoring object level permission

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -4,12 +4,14 @@ from rest_framework import viewsets
 
 from todos.models import Todo
 from todos.serializers import TodoSerializer, UserSerializer
+from todos.permissions import IsOwnerOrReadOnly
 
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
