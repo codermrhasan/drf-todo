ed40a26cf6ba7424813fe549825a3567ecdd1971
Rakib Hasan <hasanthecoder@gmail.com>
2019-10-22 22:15:16 +0600

    Refactoring TodoSerializer into ModelSerializer

--- backend/todos/serializers.py
+++ backend/todos/serializers.py
@@ -2,16 +2,7 @@ from rest_framework import serializers
 from todos.models import Todo
 
 
-class TodoSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    text = serializers.CharField(max_length=100)
-    status = serializers.BooleanField(default=False)
-
-    def create(self, validated_data):
-        return Todo.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.text = validated_data.get('text', instance.text)
-        instance.status = validated_data.get('status', instance.status)
-        instance.save()
-        return instance
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id', 'text', 'status']
